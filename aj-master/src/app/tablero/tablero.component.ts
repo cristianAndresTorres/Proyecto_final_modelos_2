735ee1c2b30c72a709fe75ec066bac742a840760
danirod29 <danielrodf29@gmail.com>
2020-08-27 06:46:52 -0500

    commit

--- aj-master/src/app/tablero/tablero.component.ts
+++ aj-master/src/app/tablero/tablero.component.ts
@@ -12,32 +12,17 @@ import {Celda} from '../Celda'
 })
 export class TableroComponent implements OnInit {
 
-  nom1=this.route.snapshot.paramMap.get('id1')
-  nom2=this.route.snapshot.paramMap.get('id2')
+  jugador1 : Jugador = {
+    id : 1,
+    nombre : "",
+    color: "negro"
+  };
+  jugador2 : Jugador = {
+    id : 2,
+    nombre : "",
+    color: "blanco"
+  };
 
-  ngOnInit() {
-
- }
-//FICHAS BLANCAS
-  PeonBlanco: Pieza ={
-    texto:"&#9817",
-  };
-  TorreBlanco: Pieza ={
-    texto:"&#9814;",
-  };
-  AlfilBlanco: Pieza ={
-    texto:"&#9815;",
-  };
-  CaballoBlanco: Pieza ={
-    texto:"&#9816;",
-  };
-  ReyBlanco: Pieza ={
-    texto:"&#9813;",
-  };
-  ReinaBlanco: Pieza ={
-    texto:"&#9812",
-  }
-  //FICHAS NEGRAS
   PeonNegro: Pieza ={
     texto:"&#9823;",
   };
@@ -60,25 +45,32 @@ export class TableroComponent implements OnInit {
     texto:"",
   }
 
-
-
-  jugador1 : Jugador = {
-    id : 1,
-    nombre : this.nom1,
-    color: "negro"
+  
+  PeonBlanco: Pieza ={
+    texto:"&#9817",
   };
-  jugador2 : Jugador = {
-    id : 2,
-    nombre : this.nom2,
-    color: "blanco"
+  TorreBlanco: Pieza ={
+    texto:"&#9814;",
   };
-
-
+  AlfilBlanco: Pieza ={
+    texto:"&#9815;",
+  };
+  CaballoBlanco: Pieza ={
+    texto:"&#9816;",
+  };
+  ReyBlanco: Pieza ={
+    texto:"&#9813;",
+  };
+  ReinaBlanco: Pieza ={
+    texto:"&#9812",
+  }
   filas = [];
- 
-  constructor(private router: Router,private route: ActivatedRoute) { 
-    
 
+  ngOnInit() {
+
+ }
+
+  constructor(private router: Router,private route: ActivatedRoute) { 
       for(var i=0; i<8; i++){
         this.filas[i] = [];
         for(var j=0; j<8; j++){
@@ -100,43 +92,28 @@ export class TableroComponent implements OnInit {
       }
 }
  inicializarTablero() {
-   //UBICAR FICHAS BLANCAS
-   //Ubicar peones
    for(var j=0;j<8;j++){
     document.getElementById("1"+j).innerHTML = this.PeonBlanco.texto;
    }
-   //Ubicar torres
    document.getElementById("00").innerHTML = this.TorreBlanco.texto;
    document.getElementById("07").innerHTML = this.TorreBlanco.texto;
-   //Ubicar alfiles
    document.getElementById("01").innerHTML = this.AlfilBlanco.texto;
    document.getElementById("06").innerHTML = this.AlfilBlanco.texto;
-   //Ubicar caballos
    document.getElementById("02").innerHTML = this.CaballoBlanco.texto;
    document.getElementById("05").innerHTML = this.CaballoBlanco.texto;
-  //Ubicar Rey
   document.getElementById("04").innerHTML = this.ReyBlanco.texto;
-  //Ubicar Reina
   document.getElementById("03").innerHTML = this.ReinaBlanco.texto;
 
-
-  //UBICAR FICHAS NEGRAS
-   //Ubicar peones
    for(var j=0;j<8;j++){
     document.getElementById("6"+j).innerHTML = this.PeonNegro.texto;
    }
-   //Ubicar torres
    document.getElementById("70").innerHTML = this.TorreNegro.texto;
    document.getElementById("77").innerHTML = this.TorreNegro.texto;
-   //Ubicar alfiles
    document.getElementById("71").innerHTML = this.AlfilNegro.texto;
    document.getElementById("76").innerHTML = this.AlfilNegro.texto;
-   //Ubicar caballos
    document.getElementById("72").innerHTML = this.CaballoNegro.texto;
    document.getElementById("75").innerHTML = this.CaballoNegro.texto;
-  //Ubicar Rey
   document.getElementById("73").innerHTML = this.ReyNegro.texto;
-  //Ubicar Reina
   document.getElementById("74").innerHTML = this.ReinaNegro.texto;
  }
 
